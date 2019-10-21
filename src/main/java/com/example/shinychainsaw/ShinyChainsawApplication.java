79a5b857ddd17e95e81d0f8f0d6311f7681272ee
Tauane Costa <tcosta@cipucrs.pucrs.br>
2019-10-21 10:22:37 -0300

    inserting archives

---
+++ src/main/java/com/example/shinychainsaw/ShinyChainsawApplication.java
@@ -0,0 +1,13 @@
+package com.example.shinychainsaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinyChainsawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinyChainsawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shinychainsaw/ShinyChainsawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shinychainsaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinyChainsawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
